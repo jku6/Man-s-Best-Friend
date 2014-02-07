6784964588efdb3b0e761730a2e351bd5071f49b
Jon <jonathanku88@gmail.com>
2014-02-07 00:31:57 -0500

    Initial Commit

---
+++ Man's Best Friend/MBFViewController.h
@@ -0,0 +1,13 @@
+//
+//  MBFViewController.h
+//  Man's Best Friend
+//
+//  Created by Jonathan Ku on 2/7/14.
+//  Copyright (c) 2014 Jonathan Ku. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBFViewController : UIViewController
+
+@end
---
+++ Man's Best Friend/MBFViewController.m
@@ -0,0 +1,29 @@
+//
+//  MBFViewController.m
+//  Man's Best Friend
+//
+//  Created by Jonathan Ku on 2/7/14.
+//  Copyright (c) 2014 Jonathan Ku. All rights reserved.
+//
+
+#import "MBFViewController.h"
+
+@interface MBFViewController ()
+
+@end
+
+@implementation MBFViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
